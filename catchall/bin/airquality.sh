5dadcbdea140129fbd8c94968a150da56adb8cb4
flippest <github@shellphi.sh>
2022-01-23 20:22:32 -0700

    minor json adjustment to add "Room" to airquality script

--- catchall/bin/airquality.sh
+++ catchall/bin/airquality.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -s airquality-gray.inside.denhac.org/data.json | jq -c '. += {"box":"textiles"}'
+curl -s airquality-gray.inside.denhac.org/data.json | jq -c '. += {"box":"textiles"}' | jq -c '. += {"Room":"textiles"}'

f2b50990f8c695e3fe5aef70ca4298d153f8d086
flippest <github@shellphi.sh>
2022-01-28 22:31:04 -0700

    Adding timestamp to unifi lines so Splunk can track each row as a separate event

--- catchall/bin/unifi.sh
+++ catchall/bin/unifi.sh
@@ -3,6 +3,9 @@
 export PASSWORD_STORE_DIR=/home/splunk/.password-store/
 export GNUPGHOME=/home/splunk/.gnupg
 
+TZ=MST
+TS=$(date +"%Y-%m-%dT%H:%M:%S")
+
 curl -sko /dev/null -X POST \
 	--data '{"username": "'$(pass show denhac/splunk/api/user)'", "password": "'$(pass show denhac/splunk/api/pass)'"}' \
 	-c /tmp/cookie.txt \
@@ -13,4 +16,4 @@ curl -sk \
 	-b /tmp/cookie.txt \
 	-H "Content-Type: application/json" \
 	https://10.11.0.2/proxy/network/api/s/default/stat/sta \
-	| jq -c '.data[] | {mac, ip, is_wired, uptime, oui}'
+	| jq -c --arg TS "${TS}" '.data[] | {timestamp: $TS, mac, ip, is_wired, uptime, oui}'
